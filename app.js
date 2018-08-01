a8b2e2000845b4fbc0714b5f89b3ec36113e2ffe
Impactionplatform <Chadbowen248@gmail.com>
2018-08-01 19:21:19 -0400

    progress a bit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const buildDropDownMarkUp = arr => arr.map((item, index) => dropdownMarkup(item, index)).join('');
+const buildResultsMarkUp = arr => arr.map((item, index) => resultsMarkup(item, index)).join('');
 
 const getCatergories = arr => object => object[arr];
-const dropdownMarkup = (item, index) => `<li class='list-item'>
+const dropdownMarkup = () => `<li class='list-item'>
                                             <input type='checkbox'
                                                 id='${item}-${index}'
                                                 value='${item}'
@@ -8,19 +10,8 @@ const dropdownMarkup = (item, index) => `<li class='list-item'>
                                             <label for='${item}-${index}'>${item}</label>
                                         </li>`
 
-
-const buildMarkUp = arr => arr.map((item, index) => dropdownMarkup(item, index)).join('');
-
-
-// const buildMarkUp = arr => {
-//   return arr.map((item, index) => `<li class='list-item'>
-//                                     <input type='checkbox'
-//                                         id='${item}-${index}'
-//                                         value='${item}'
-//                                         class='list-style'>
-//                                     <label for='${item}-${index}'>${item}</label>
-//                               </li>`).join('');
-//                             }
+const resultsMarkup = (item, index) => `<li>${item}-${index}</li>`
+//TODO: Figure out how to not repeat this, should be able to just pass in function, but params are not defined
 
 const appendToParent = node => html => {
     const ele = document.getElementById(node);
@@ -33,7 +24,7 @@ const inspect = a => {
   }
 
 const pipe = (...fns) => start => fns.reduce((x,y) => y(x), start);
-const renderMarkup = category => pipe(getCatergories(category), buildMarkUp, appendToParent(category), inspect);
+const renderMarkup = category => pipe(getCatergories(category), buildDropDownMarkUp, appendToParent(category));
 const populateDropDowns = (res) => {
     renderMarkup('categories')(res);
     renderMarkup('programs')(res);
@@ -54,15 +45,15 @@ const addRemoveClass = (options) => e => {
 const toggleDropdowns = addRemoveClass({className:'show', applyToSibling: true})
 
 const addListeners = (options) => {
-    const {type, className, fn} = options
-    const eles = [].slice.call(document.querySelectorAll(className));
+    const {type, selector, fn} = options
+    const eles = document.querySelectorAll(selector);
     eles.forEach(item => {
         item.addEventListener(type, fn)
     })
 }
 
 addListeners({type: 'click',
-            className: '.dropdowns',
+            selector: '.dropdowns',
             fn: toggleDropdowns});
 
-document.body.addEventListener('click', e => console.log(e.target))
+// document.body.addEventListener('click', e => console.log(e.target))
