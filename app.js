5acc4b5ad29a4f45379025e5d4f87075cf945078
Impactionplatform <Chadbowen248@gmail.com>
2018-08-01 19:22:32 -0400

    more

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const buildDropDownMarkUp = arr => arr.map((item, index) => dropdownMarkup(item, index)).join('');
-const buildResultsMarkUp = arr => arr.map((item, index) => resultsMarkup(item, index)).join('');
 
 const getCatergories = arr => object => object[arr];
-const dropdownMarkup = () => `<li class='list-item'>
+const dropdownMarkup = (item, index) => `<li class='list-item'>
                                             <input type='checkbox'
                                                 id='${item}-${index}'
                                                 value='${item}'
@@ -12,6 +10,8 @@ const dropdownMarkup = () => `<li class='list-item'>
 
 const resultsMarkup = (item, index) => `<li>${item}-${index}</li>`
 //TODO: Figure out how to not repeat this, should be able to just pass in function, but params are not defined
+const buildDropDownMarkUp = arr => arr.map((item, index) => dropdownMarkup(item, index)).join('');
+const buildResultsMarkUp = arr => arr.map((item, index) => resultsMarkup(item, index)).join('');
 
 const appendToParent = node => html => {
     const ele = document.getElementById(node);
