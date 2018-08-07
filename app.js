dbecf4ad1ed19a88485fddd0647ef9ea6bc77d94
cbowen-aarp <cbowen@aarp.org>
2018-08-07 13:19:50 -0400

    about to mess shit up

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
+
+
 const getCatergories = arr => object => object[arr];
 
 const buildDropDownMarkUp = arr => arr.map((item, index) =>  `<li class='list-item'>
                                                                 <input type='checkbox'
                                                                     id='${item}-${index}'
                                                                     value='${item}'
-                                                                    class='list-style'>
+                                                                    class='list-style list-checked' checked>
                                                                 <label for='${item}-${index}'>${item}</label>
                                                               </li>`).join('');
 
@@ -27,6 +29,10 @@ const populateDropDowns = (res) => {
     renderMarkup('programs')(res);
 }
 
+// const getVTMdata = fetch('https://services.share.aarp.org/applications/CoreServices/WSOWebService/vtmresource/jobdata')
+//                 .then(res => res.json())
+//                 .then(res => (res.data))
+//                 .then(res => populateDropDowns(res))
 const getVTMdata = fetch('programdata.json')
                 .then(res => res.json())
                 .then(res => populateDropDowns(res))
@@ -56,8 +62,29 @@ const closeDropdownOutsideClick = (id, dropdown) => e => {
 const closeDropDownPrograms = closeDropdownOutsideClick('#programs11','programs');
 const closeDropDownInterests = closeDropdownOutsideClick('#interests11','categories');
 
+const getCheckedValues = nodelist => {
+    const arr = [];
+    nodelist.forEach(item => {
+        arr.push(encodeURIComponent(item.value));
+    })
+    return arr
+}
+
+const getInputValue = input => input.value
+
+const postSearchData = e => {
+    e.preventDefault();
+    const interestArr = getCheckedValues(document.querySelectorAll('#categories input[type=checkbox]:checked'));
+    const programsArr = getCheckedValues(document.querySelectorAll('#programs input[type=checkbox]:checked'));
+    const zipcode = getInputValue(document.getElementById('postalCode'));
+    const distance = getInputValue(document.getElementById('zipcode'));
+    console.log(interestArr, programsArr, zipcode, distance)
+}
+
 addListeners({type: 'click', selector: '.dropdowns', fn: toggleDropdowns});
 
 addListeners({type: 'click', selector: 'body', fn: closeDropDownPrograms});
 
 addListeners({type: 'click', selector: 'body', fn: closeDropDownInterests});
+
+addListeners({type: 'click', selector: '#test', fn: postSearchData});
